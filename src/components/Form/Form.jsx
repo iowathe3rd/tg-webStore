a319b43f18ff69a71c3b6853e9e993b74e374073
iowathe3rd <beglerov04@mail.ru>
2022-10-11 20:45:20 +0600

    added form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import "./form.css";
+
+function Form() {
+	const [country, setCountry] = useState("");
+	const [street, seStreet] = useState("");
+	const [subject, setSubject] = useState("physical");
+	const { tg } = useTelegram();
+
+	useEffect(() => {
+		tg.MainButton.setParams({
+			text: "Отправить",
+		});
+	}, []);
+
+	useEffect(() => {
+		if (!street || !country) {
+			tg.MainButton.hide();
+		} else {
+			tg.MainButton.show();
+		}
+	});
+
+	const onChangeCountry = (e) => {
+		setCountry(e.target.value);
+	};
+
+	const onChangeStreet = (e) => {
+		seStreet(e.target.value);
+	};
+
+	const onChangeSubject = (e) => {
+		setSubject(e.target.value);
+	};
+
+	return (
+		<div className="form">
+			<h3>Введите ваши данные</h3>
+			<input
+				className={"input"}
+				type={"text"}
+				placeholder={"Страна"}
+				value={country}
+				onChange={onChangeCountry}
+			/>
+			<input
+				className={"input"}
+				type={"text"}
+				placeholder={"Улица"}
+				value={street}
+				onChange={onChangeStreet}
+			/>
+
+			<select className={"select"} value={subject} onChange={onChangeSubject}>
+				<option value={"physical"}>Физ лицо</option>
+				<option value={"legal"}>Юр лицо</option>
+			</select>
+		</div>
+	);
+}
+
+export default Form;
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./productList.css";
+
+function ProductList() {
+	return <div>ProductList</div>;
+}
+
+export default ProductList;
